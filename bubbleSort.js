2ef8f2bf1abbaebb778c2e2630069c3bb152f329
rajesh-paudel <rajeshpaudel9863@gmail.com>
2025-02-16 17:47:46 +0545

    bubble sort

---
+++ bubbleSort.js
@@ -0,0 +1,14 @@
+//bubble sort
+
+let arr = [1, 34, 23, 45, 55, 67, 87, 97, 46, 64, 46, 76];
+
+for (i = 0; i < arr.length; i++) {
+  for (j = 0; j < arr.length; j++) {
+    if (arr[j] > arr[j + 1]) {
+      let temp = arr[j];
+      arr[j] = arr[j + 1];
+      arr[j + 1] = temp;
+    }
+  }
+}
+console.log(arr);
