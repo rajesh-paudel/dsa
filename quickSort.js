ffe5c18c81554b8023a96426ca015296d857ec2f
rajesh-paudel <rajeshpaudel9863@gmail.com>
2025-02-25 19:24:17 +0545

    quick sort

--- quickSort.js
+++ quickSort.js
@@ -1,19 +1,24 @@
 const array = [2, 3, 32, 243, 23, 12, 43, 632, 4312, 13];
 
-function quickSort(arr) {
-  let pivot = arr[0];
-  let p = pivot + 1;
-  let q = arr.length - 1;
-
-  while (p < q) {
-    if (arr[p] < pivot) {
-      p = p + 1;
-    }
-    if (arr[q] > pivot) {
-      q = q - 2;
-    }
-    let temp = arr[p];
-  }
+function quickSort(arr, start, end) {
+  if (end <= start) return;
+  let pivotPos = partition(arr, start, end);
+  quickSort(arr, start, pivotPos - 1);
+  quickSort(arr, pivotPos + 1, end);
 }
-quickSort(array);
+function partition(arr, start, end) {
+  let pivot = arr[end];
+  let i = start - 1;
+  for (j = start; j <= end; j++) {
+    if (arr[j] <= pivot) {
+      i = i + 1;
+      temp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = temp;
+    }
+  }
+  return i;
+}
+
+quickSort(array, 0, array.length - 1);
 console.log(array);
