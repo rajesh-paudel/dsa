7fec3b794cb66465e70fcb77a81fd713f653e927
rajesh-paudel <rajeshpaudel9863@gmail.com>
2025-03-13 19:18:23 +0545

    improve bubblesort

--- bubbleSort.js
+++ bubbleSort.js
@@ -3,7 +3,7 @@
 let arr = [1, 34, 23, 45, 55, 67, 87, 97, 46, 64, 46, 76];
 
 for (i = 0; i < arr.length; i++) {
-  for (j = 0; j < arr.length; j++) {
+  for (j = 0; j < arr.length - i - 1; j++) {
     if (arr[j] > arr[j + 1]) {
       let temp = arr[j];
       arr[j] = arr[j + 1];
